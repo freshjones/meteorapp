773d53fd4ec2a91838d06c552c17eba7a861e43a
WIlliam Jones <billy@freshjones.com>
2013-09-11 07:12:38 -0400

    fixing dir structure

--- collections.js
+++
@@ -1,5 +0,0 @@
-Features = new Meteor.Collection('features'); 
-Clients = new Meteor.Collection('clients'); 
-Projects = new Meteor.Collection('projects'); 
-Settings = new Meteor.Collection('settings');
-Iterations = new Meteor.Collection('iterations'); 
